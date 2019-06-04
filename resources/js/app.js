a91c4ab7846e6ecabf3267c8f55e854535387c94
sustained <sustained.dissonance@gmail.com>
2019-06-04 01:12:45 +0100

    Refactoring/formatting.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,24 @@
+// require("./bootstrap");
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from "vue";
 
-require('./bootstrap');
+const files = require.context("./components", true, /Base[A-Z]\w+\.(vue|js)$/i);
 
-window.Vue = require('vue');
+files.keys().map(key => {
+  Vue.component(
+    key
+      .split("/")
+      .pop()
+      .split(".")[0],
+    files(key).default
+  );
+});
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import router from "./router/router.js";
+import store from "./store/store.js";
 
 const app = new Vue({
-    el: '#app'
+  store,
+  router,
+  el: "#app"
 });
