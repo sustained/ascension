5b87f48e8865170c6b4bff2b093a77a307a04109
sustained <sustained.dissonance@gmail.com>
2019-06-08 03:34:25 +0100

    Check for admin email address in gates.

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -63,7 +63,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                //
+                env('ADMIN_EMAIL_ADDRESS')
             ]);
         });
     }
