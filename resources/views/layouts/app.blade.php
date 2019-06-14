83907aa8eb80c6a57f7ae205e482e213282b1d28
sustained <sustained.dissonance@gmail.com>
2019-06-14 03:56:38 +0100

    Remove default Bootstrap stuff & add own styling.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,68 +14,62 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app" :class="themeClass">
-        <nav class="bg-header text-default">
-            Ascension
-
-            <button class="btn blue" @click="toggleTheme()">Toggle Theme</button>
-        </nav>
-
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="display: none;">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+        <nav class="nav-main">
+            <h1 class="nav-brand">
+                <a href="{{ url('/') }}">
                     {{ config('app.name', 'Ascension') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            </h1>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            {{-- <button class="btn blue" @click="toggleTheme()">Toggle Theme</button> --}}
 
-                    </ul>
+            <ul>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                <li>
+                    <a href="/learn/courses">Courses</a>
+                </li>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                <li>
+                    <a href="/forums">Forums</a>
+                </li>
+            </ul>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
+            @guest
+            <ul>
+                <li>
+                    <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+
+                @if (Route::has('register'))
+                <li>
+                    <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @endif
+            </ul>
+            @else
+            <ul>
+                <li>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+            @endguest
         </nav>
 
         <main class="bg-container">
