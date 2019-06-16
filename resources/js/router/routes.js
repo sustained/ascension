079c02c992970607313ffb842efdedb403a2a9aa
sustained <sustained.dissonance@gmail.com>
2019-06-16 17:34:05 +0100

    Add new user/auth routes to vue-router.

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -3,9 +3,15 @@ import Course from "../views/ViewCourse.vue";
 import Level from "../views/ViewLevel.vue";
 import Courses from "../views/ListCourses.vue";
 import NotFound from "../views/NotFound.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import AuthTest from "../views/AuthTest.vue";
 
 export default [
   { path: "/", component: Home, name: "home" },
+  { path: "/user/login", component: Login, name: "login" },
+  // { path: "/register", component: Register, name: "register" },
+  { path: "/auth-test", component: AuthTest, name: "auth-test", meta: { requiresAuth: true } },
   { path: "/courses", component: Courses, name: "courses" },
   { path: "/courses/:course_id", component: Course, name: "course", props: true },
   {
