526485b4f0229436aa0b0927c33823271ef398b6
sustained <sustained.dissonance@gmail.com>
2019-06-16 09:39:35 +0100

    Use router-links for login/register.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,12 +48,12 @@
             @guest
             <ul>
                 <li>
-                    <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <router-link to="/login">{{ __('Login') }}</router-link>
                 </li>
 
                 @if (Route::has('register'))
                 <li>
-                    <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                    <router-link to="/register">{{ __('Register') }}</router-link>
                 </li>
                 @endif
             </ul>
