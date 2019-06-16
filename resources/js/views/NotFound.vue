ccaa00de5f4390f4c15f8dcfcfb5dbc6f185786d
sustained <sustained.dissonance@gmail.com>
2019-06-16 17:33:19 +0100

    Prettify 404 view.

--- resources/js/views/NotFound.vue
+++ resources/js/views/NotFound.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <h1>404</h1>
-    <p>Page not found!</p>
+  <div class="container container-fixed">
+    <div class="center-full">
+      <div class="w-1/2">
+        <div class="card bg-discord-bg-1 shadow-lg">
+          <h1>404</h1>
+          <p>Page not found!</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
