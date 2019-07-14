ffba029ac428652ae8bc11688b9542b99441584e
sustained <sustained.dissonance@gmail.com>
2019-07-14 19:27:21 +0100

    No longer send user - use /api/auth/me instead.

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -99,7 +99,6 @@ class JWTAuthController extends Controller
             'access_token' => $token,
             'token_type'   => 'bearer',
             'expires_in'   => auth()->factory()->getTTL() * 60,
-            'user' =>  auth()->user()
         ]);
     }
 }
